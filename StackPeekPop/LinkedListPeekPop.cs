ddfe88d116c842bd2f4218bd1fd576c68f473910
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-15 21:38:27 +0530

    [Prashant]:Ability to peek and pop from the stack

---
+++ StackPeekPop/LinkedListPeekPop.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackPeekPop
+{
+    public class LinkedListPeekPop
+    {
+        public Node top;
+        public LinkedListPeekPop()
+        {
+            this.top = null;
+
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top != null)
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to Stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of stack", this.top.data); 
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("value popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while(this.top!= null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+
+    }
+}
+
---
+++ StackPeekPop/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackPeekPop;
+using System;
+namespace StackPeepPop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListPeekPop linkedListPeekPop = new LinkedListPeekPop();
+            linkedListPeekPop.Push(70);
+            linkedListPeekPop.Push(30);
+            linkedListPeekPop.Push(56);
+            linkedListPeekPop.Display();
+            linkedListPeekPop.Peek();
+            linkedListPeekPop.Pop();
+            linkedListPeekPop.IsEmpty();
+            
+        }
+    }
+}
