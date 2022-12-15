162de6b51cb61f5533c12b335fe2048bd9c599fc
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-15 21:35:22 +0530

    [Prashant]: Create stack and call add method

---
+++ StackProblem/LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblem
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public LinkedListStack()
+        {
+            this.top= null;
+
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top != null)
+            {
+                node.next= this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to Stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp= temp.next;
+            }
+        }
+
+
+
+    }
+}
---
+++ StackProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace StackProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
