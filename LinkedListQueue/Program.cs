749a356f0234bbf4f08cd054f8e3f16e04eb11af
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-15 21:40:41 +0530

    [Prashant]:Create a Queue

---
+++ LinkedListQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedListQueue
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListEnqueue linkedListEnqueue = new LinkedListEnqueue();
+            linkedListEnqueue.Enqueue(56);
+            linkedListEnqueue.Enqueue(30);
+            linkedListEnqueue.Enqueue(70);
+            linkedListEnqueue.Display();
+
+        }
+    }
+}
