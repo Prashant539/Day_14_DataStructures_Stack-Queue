0a787a0640f955fbaeb5717f70840f3c140f03fb
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-15 21:43:16 +0530

    [Prashant]:Create a Dequeue LinkedList

---
+++ LinkedListDequeue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LinkedListDequeue
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedDeQueue linkedDeQueue = new LinkedDeQueue();
+            linkedDeQueue.Dequeue(70);
+            linkedDeQueue.Dequeue(30);
+            linkedDeQueue.Dequeue(50);
+            linkedDeQueue.Display();
+          
+           
+
+        }
+    }
+}
